f9b2895c3a1a45a0b938f3fbbe180421d219e7da
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-03-17 08:21:51 -0500

    :bento: Getting started with styles

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        padding: 40,
+      }}
+    >
+      <View>
+        <TextInput
+          placeholder='Course Goal'
+          style={{
+            borderRadius: 8,
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+            marginBottom: 8,
+          }}
+        />
+        <Button title='ADD' />
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
