074350bf07b933f554f9fb11c66ef2bce7950371
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-03-23 07:34:46 -0500

    :bento: 28 - Outputting a List of items

--- App.js
+++ App.js
@@ -33,7 +33,11 @@ export default function App() {
         <Button title='ADD' onPress={addGoalHandler} />
       </View>
 
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+      </View>
 
       <StatusBar style='auto' />
     </View>
