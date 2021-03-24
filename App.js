da91cec55f448f704e4cd86b67b97a58519ef886
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-03-24 07:50:58 -0500

    :bento: 36 - Adding a modal overlay

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-} from 'react-native';
+import { Button, StyleSheet, View, FlatList } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = (newGoal) => {
     setCourseGoals((currentGoals) => [
@@ -33,7 +27,8 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title='ADD NEW GOAL' onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} />
 
       <FlatList
         keyExtractor={(item, index) => item.id}
