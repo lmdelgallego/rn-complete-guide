18755e6ace71f026943bce7e9e991678f302a8cb
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-04-06 07:41:43 -0500

    :bento: 75 - Adding Local Image

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -1,14 +1,17 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import TitleText from '../components/TitleText';
+import BodyText from '../components/BodyText';
 
 // create a component
 const GameOverScreen = (props) => {
   return (
     <View style={styles.container}>
-      <Text>The Game is Over!!!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>The Game is Over!!!</TitleText>
+      <Image source={require('../assets/success.png')} />
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title='NEW GAME' onPress={props.onRestart} />
     </View>
   );
@@ -20,7 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
   },
 });
 
