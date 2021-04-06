504d74362ca9ec4f4c15368381c015970365060c
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-04-06 08:43:11 -0500

    :bento: 78 - A close look at the 'Tetex' component

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
+import Colors from '../constans/Colors';
 
 // create a component
 const GameOverScreen = (props) => {
@@ -20,8 +21,14 @@ const GameOverScreen = (props) => {
           resizeMode='cover'
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{' '}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{' '}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
       <Button title='NEW GAME' onPress={props.onRestart} />
     </View>
   );
@@ -46,6 +53,18 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  resultContainer: {
+    marginHorizontal: 90,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
   },
 });
 
