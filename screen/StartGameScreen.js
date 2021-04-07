2902333e5c253b3f7604b5492c9f55da29c9507f
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-04-07 08:11:03 -0500

    :bento: 92 - Indroducing the Dimensions API

--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -3,11 +3,11 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput,
   Button,
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/input';
@@ -132,7 +132,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    // width: 100,
+    width: Dimensions.get('window').width / 4, // 40%
   },
   summaryContainer: {
     marginTop: 20,
