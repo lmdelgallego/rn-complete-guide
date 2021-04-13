a520b9bcda808bab13b676ab6fae8f7b689f32f2
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-04-13 08:03:10 -0500

    :bento: 104 - Using Platform-specific code files

---
+++ components/MainButton.ios.js
@@ -0,0 +1,33 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Colors from '../constants/colors';
+
+// create a component
+const MainButton = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+      <View style={{ ...styles.button, ...props.style }}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: 'white',
+    fontFamily: 'open-sans',
+    fontSize: 18,
+  },
+});
+
+//make this component available to the app
+export default MainButton;
