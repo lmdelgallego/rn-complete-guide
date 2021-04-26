27a52389f2e0651bd655cb0a79e2634603be8f4e
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-04-26 07:48:41 -0500

    :bento: 127 - Grid Styling & Some Refactoring

--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -6,24 +6,22 @@ import {
   View,
   TouchableOpacity,
 } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 import { CATEGORIES } from '../data/dummy-data';
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate('CategoryMeals', {
             categoryId: itemData.item.id,
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -48,10 +46,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
