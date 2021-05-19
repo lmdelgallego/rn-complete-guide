efb85e3a983144122c5bda788f6e15f32125bed6
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-05-19 07:05:41 -0500

    blank project

--- App.js
+++ App.js
@@ -1,76 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
-
-import Header from './components/Header';
-import GameOverScreen from './screen/GameOverScreen';
-import GameScreen from './screen/GameScreen';
-import StartGameScreen from './screen/StartGameScreen';
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-};
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
-  const configureNewGameHandler = () => {
-    setGuessRounds(0);
-    setUserNumber(null);
-  };
-
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-    setGuessRounds(0);
-  };
-
-  const gameOverHandler = (numOfRounds) => {
-    setGuessRounds(numOfRounds);
-  };
-
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
-  if (userNumber && guessRounds <= 0) {
-    content = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
-  } else if (guessRounds > 0) {
-    content = (
-      <GameOverScreen
-        roundsNumber={guessRounds}
-        userNumber={userNumber}
-        onRestart={configureNewGameHandler}
-      />
-    );
-  }
-
   return (
-    <SafeAreaView style={styles.screen}>
-      <Header title='Guess a Number' />
-      {content}
-      <StatusBar style='auto' />
-    </SafeAreaView>
+    <View>
+      <Text>A new app!</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-});
