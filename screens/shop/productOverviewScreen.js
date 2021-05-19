32a1633979e763e47cf223c249cb69eb58b40676
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-05-19 07:32:09 -0500

    :bento: 166. The Products Overview Screen

---
+++ screens/shop/productOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const ProductOverviewScreen = (props) => {
+  return <FlatList />;
+};
+
+export default ProductOverviewScreen;
+
+const styles = StyleSheet.create({});
