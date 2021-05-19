4b1af731b722f0c3f59b0d29dd5c77de05cc9697
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-05-19 07:53:38 -0500

    :bento: 167. Setting Up a Navigator

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -14,7 +15,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <Text>A new app!</Text>
+      <ShopNavigator />
     </Provider>
   );
 }
