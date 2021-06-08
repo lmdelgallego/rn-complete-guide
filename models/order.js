4ba557f01926496e53fd7e7e112289559a0399b4
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-06-08 11:29:39 -0500

    :bento: 179. Styling Order Items

--- models/order.js
+++ models/order.js
@@ -1,9 +1,16 @@
+import moment from 'moment';
+
 class Order {
   constructor(id, items, amount, date) {
     this.id = id;
     this.items = items;
     this.amount = amount;
     this.date = date;
+  }
+
+  get readableDate() {
+    // return 'asdkljmasdjk';
+    return moment(this.date).format('MMMM Do YYYY, h:mm');
   }
 }
 
