4218b5155faaf3d848c1f27d3498f1f0144cc9ca
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-06-21 09:51:18 -0500

    :bento: 204. Storing Products on a Server

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,15 +7,38 @@ export const deleteProduct = (productId) => ({
   pid: productId,
 });
 
-export const createProduct = (title, description, imageUrl, price) => ({
-  type: CREATE_PRODUCT,
-  productData: {
-    title: title,
-    description: description,
-    imageUrl: imageUrl,
-    price: price,
-  },
-});
+export const createProduct = (title, description, imageUrl, price) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://goalcoach-a4187.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: responseData.name,
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+      },
+    });
+  };
+};
 
 export const updateProduct = (id, title, description, imageUrl) => ({
   type: UPDATE_PRODUCT,
