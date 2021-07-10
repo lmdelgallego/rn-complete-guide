2db97ea3f079fbf8073fe94ebdacdf66b18f3db3
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-07-10 13:44:09 -0500

    :bento:210. Storing Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,32 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      'https://goalcoach-a4187.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: responseData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
