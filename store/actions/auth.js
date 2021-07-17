21b3c765d22c97e8ab39f9f6998bbbdb8b9c0b14
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-07-17 08:26:50 -0500

    :bento: 220. Logging Users In

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 const API_KEY = 'AIzaSyArFtSOsqmL0k31tCgy47dPKtffXTasr1o';
 
 export const signup = (email, password) => {
@@ -23,5 +24,30 @@ export const signup = (email, password) => {
     const resData = await response.json();
     console.log(resData);
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error('Something went wrong!!');
+    }
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
   };
 };
