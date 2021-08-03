b15422a5eacb92c99c3f27fe938352f9223904ae
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-08-02 20:10:49 -0500

    :bento: 242. Storing the Image on the Filesystem

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -15,7 +15,7 @@ const PlacesListScreen = (props) => {
       keyExtractor={(item) => item.id}
       renderItem={(itemData) => (
         <PlaceItem
-          image={imageData.item.imageUrl}
+          image={itemData.item.imageUrl}
           title={itemData.item.title}
           address={null}
           onSelect={() => {
