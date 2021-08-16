34e0044b50cc2b22e2d369b30576d334c428e5e9
Luis Miguel Del Gallego <lmdelgallego@gmail.com>
2021-08-16 13:00:39 -0500

    :bento: 283 More Information & Updating the Project - ReactRouterNative

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProductOverviewScreen from '../screens/shop/ProductOverviewScreen';
+
+// import ShopNavigator from './ShopNavigator';
+// const MyStack = createStackNavigator();
+const MyStack = createNativeStackNavigator();
+
+const AppNavigator = (props) => {
+  const isAuth = useSelector((state) => !!state.auth.token);
+
+  return (
+    <NavigationContainer>
+      <MyStack.Navigator>
+        <MyStack.Screen
+          name='ProductsOverview'
+          component={ProductOverviewScreen}
+        />
+      </MyStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
