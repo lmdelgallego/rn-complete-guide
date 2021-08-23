26b8f7d79359784e40b1239ad3716b8eb5a48429
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-08-23 11:46:35 -0500

    :bento: 300. Sending Local Notifications

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Button, View, StyleSheet } from 'react-native';
+import * as Notification from 'expo-notifications';
 
 export default function App() {
+  const triggerNotificationsHandler = () => {
+    Notification.scheduleNotificationAsync({
+      content: {
+        title: 'Fist local notification',
+        body: 'This is a local notification we are sending',
+      },
+      trigger: {
+        seconds: 10,
+      },
+      identifier: '',
+    });
+  };
+
   return (
-    <View>
-      <Text>A new app!</Text>
+    <View style={styles.container}>
+      <Button
+        title='Trigger Notification'
+        onPress={triggerNotificationsHandler}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
